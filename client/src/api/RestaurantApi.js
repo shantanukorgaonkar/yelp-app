92f81b2062cb748ecd9e177ff6b6c8a981aae33e
Shantanu Korgaonkar <shantanukorgaonkar7@gmail.com>
2022-08-28 12:43:44 +0530

    initial commit

--- client/src/api/RestaurantApi.js
+++ client/src/api/RestaurantApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === "production"?"/api/v1/restaurants" :"http://localhost:4000/api/v1/restaurants";
+const baseURL = process.env.NODE_ENV === "production"?"https://yelp-app-pern.herokuapp.com/api/v1/restaurants" :"http://localhost:4000/api/v1/restaurants";
 
 export default axios.create({
     baseURL
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const restaurantRoutes = require('./routes/RestaurantRoutes.js')
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,15 @@ app.use(cors())
 app.use(express.json());
 
 app.use('/api/v1/restaurants',restaurantRoutes);
+
+if(process.env.NODE_ENV==='production'){
+app.use(express.static('client/build'));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+})
+
+}
 
 app.listen(PORT, ()=>{
     console.log(`Server is up and listening on port  ${PORT}`);
